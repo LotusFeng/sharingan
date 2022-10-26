6ce1e9cd1117dbdcc76e1d477f8deab33b57726b
czf <505487452@qq.com>
2022-10-26 15:25:37 +0800

    修复bug

--- src/css.js
+++ src/css.js
@@ -1,112 +1,114 @@
 const string = `
 <style>
-.sharingan *{box-sizing: border-box;padding: 0;margin: 0;}
-.sharingan{
-    background: rgb(93, 58, 58);
-    min-height: 50vh;
-    position: relative;
-}
-.eye{
-    position: absolute;
-    left: 50%;
-    margin-left: -125px;
-    top: 100px;
-    background: black;
-    border-radius: 50%;
+body{
+    background:#5d3a3a;
+  }
+  div{
+    position:absolute;
+  }
+#sasuke-ems{
+    border-radius:100%;
+    height:250px;
     width: 250px;
-    height: 250px;
-}
-.flower1,.flower2,.flower3{
-    position: absolute;
-    background: rgb(160, 33, 50);
-    border-radius: 100% 0;
-    top: 145px;
-    width: 160px;
-    height: 160px;
-    margin-left: -80px;
-    left: 50%;
-    border: 4px solid black;
-    animation: glowing 3s infinite;
-}
-.flower1{
-    rotate: -45deg;
-}
-.flower2{
-    rotate: -105deg;
-}
-.flower3{
-    rotate: -165deg;
-}
-.flower-line1, .flower-line2, .flower-line3{
-    position: absolute;
-    left: 50%;
-    margin-left: -80px;
-    border: 4px solid black;
+    background:black;
+	  animation: rotation 2s infinite;
+  }
+#sasuke-ems-flower-1,#sasuke-ems-flower-2,#sasuke-ems-flower-3{
+    background:#a02132;
     height:160px;
     width:160px;
-    border-radius: 100% 0;
-    top: 145px;
-}
-.flower-line1{
-    rotate: -45deg;
-}
-.flower-line2{
-    rotate: -105deg;
-}
-.flower-line3{
-    rotate: -165deg;
-}
-
-.itachi-1-1,.itachi-2-1,.itachi-3-1{
-    border-radius: 100% 0px 0px;
-}
-.itachi-1-1,.itachi-2-1,.itachi-3-1,.itachi-1-2,.itachi-2-2,.itachi-3-2{
-    position: absolute;
-    width: 20px;
-    height: 100px;
-    background: black;
-}
-.itachi-1-2,.itachi-2-2,.itachi-3-2{
-    left: 18px;
-    border-radius: 0px 100% 0px 0px;
-}
-.itachi-1-1{
-    left: 50%;
-    margin-left: -41px;
-    rotate: -60deg;
-    top: 168px;
-}
-.itachi-2-1{
-    left: 50%;
-    rotate: 60deg;
-    margin-left: 21px;
-    top: 149px;
-}
-.itachi-3-1{
-    left: 50%;
-    rotate: 180deg;
-    top: 214px;
-    margin-left: -1px;
-}
-.pupil{
-    position: absolute;
-    background: rgb(160, 33, 50);
-    width: 35px;
-    height: 35px;
-    border-radius: 50%;
-    left: 50%;
-    margin-left: -17.5px;
-    top: 207.5px;
-}
-@keyframes glowing {
-    0% { box-shadow: 0 0 20px #ff1c20; }
-    25% { box-shadow: 0 0 10px ; }
-      50% { box-shadow: 0 0  0px #ff1c20; }
-      75% { box-shadow: 0 0 10px #ff1c20; }
-    100% { box-shadow: 0 0 20px #ff1c20; }
+		border:4px solid #a02132;
+    animation: glowing 3s infinite;
+  }
+#sasuke-ems-flower-line-1,#sasuke-ems-flower-line-2,#sasuke-ems-flower-line-3{
+    height:160px;
+    width:160px;
+		border:4px solid black;
+  }
+#sasuke-ems-flower-line-1{
+    transform:rotate(-45deg);
+  }
+#sasuke-ems-flower-line-2{
+    transform:rotate(-105deg);
+      
+  }
+#sasuke-ems-flower-line-3{
+    transform:rotate(-165deg);
   }
 
-</style>
+ 
+#sasuke-ems-flower-1,#sasuke-ems-flower-2,#sasuke-ems-flower-3,#sasuke-ems-flower-line-1,#sasuke-ems-flower-line-2,#sasuke-ems-flower-line-3{
+    top:42px;
+    left:42px;
+    border-radius: 100% 0;
+    
+  }
+  #sasuke-ems,{
+    position:sticky;
+    margin:auto;
+  margin-bottom:30px;
+}
+  #sasuke-ems-flower-1{
+    transform:rotate(-45deg);
+  }
+    #sasuke-ems-flower-2{
+    transform:rotate(-105deg);
+      
+  }
+  #sasuke-ems-flower-3{
+    transform:rotate(-165deg);
+  }
+  #sasuke-ems-flower-bullet-1-1,#sasuke-ems-flower-bullet-2-1,#sasuke-ems-flower-bullet-3-1{
+  	border-radius: 100%  0px 0px ;
+  }
+  #sasuke-ems-flower-bullet-1-1,#sasuke-ems-flower-bullet-2-1,#sasuke-ems-flower-bullet-3-1,#sasuke-ems-flower-bullet-1-2,#sasuke-ems-flower-bullet-2-2,#sasuke-ems-flower-bullet-3-2{
+    width: 20px; 
+  	height: 100px; 
+  	background:black;
+  }
+  #sasuke-ems-flower-bullet-1-2,#sasuke-ems-flower-bullet-2-2,#sasuke-ems-flower-bullet-3-2{
+    left:18px;
+  	border-radius: 0px 100% 0px 0px;
+  }
+  #sasuke-ems-flower-bullet-1-1{
+    transform:rotate(-60deg);
+    top:65px;
+    left:80px;
+  }
+  #sasuke-ems-flower-bullet-2-1{
+    transform:rotate(60deg);
+    top:47px;
+    left:150px;
+  }
+  #sasuke-ems-flower-bullet-3-1{
+    transform:rotate(180deg);
+    top:120px;
+    left:125px; 
+  }
+  #sasuke-ems-pupil{
+    background:#a02132;
+    height:35px;
+    width:35px;
+    top:108px;
+    left:108px;
+    border-radius:100%;
+  }
+@keyframes rotation {
+		from {
+				transform: rotate(0deg);
+		}
+		to {
+				transform: rotate(360deg);
+		}
+}
+@keyframes glowing {
+  0% { box-shadow: 0 0 20px #ff1c20; }
+  25% { box-shadow: 0 0 10px ; }
+	50% { box-shadow: 0 0  0px #ff1c20; }
+	75% { box-shadow: 0 0 10px #ff1c20; }
+  100% { box-shadow: 0 0 20px #ff1c20; }
+}
+<style/>
 `
 
 export default string;
